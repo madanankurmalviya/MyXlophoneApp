16f778f803625c102a86519a150922bb0ef33b47
Ankur Malviya <madanankurmalviya@gmail.com>
2023-11-03 21:13:07 -0700

    First commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
-
-
+    
+    @IBAction func aButton(_ sender: UIButton) {
+        print("A button is clicked...")
+//        playSound()
+    }
+    
+    @IBAction func bButton(_ sender: UIButton) {
+        print("B button is clicked...")
+    }
+    @IBAction func cButton(_ sender: UIButton) {
+        print("C button is clicked...")
+    }
+    @IBAction func d_Button(_ sender: UIButton) {
+        print("D button is clicked...")
+    }
+    @IBAction func eButton(_ sender: UIButton) {
+        print("E button is clicked...")
+    }
+//    func playSound(){
+//        let url = Bundle.main.url(forResource: "/Users/yashaswi/Downloads/test", withExtension: "MOV")
+//        player = try! AVAudioPlayer(contentsOf: url!)
+//        player.play()
+//    }
 }
 
