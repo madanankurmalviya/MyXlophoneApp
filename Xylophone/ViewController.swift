d187ae1f6555a640d3ddfbebdf893b82a99f8ae3
Ankur Malviya <madanankurmalviya@gmail.com>
2023-11-04 05:14:30 -0700

    added button logic

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -19,26 +19,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func aButton(_ sender: UIButton) {
-        print("A button is clicked...")
-//        playSound()
+
+        print(sender.titleLabel as Any)
+        
+        
     }
-    
-    @IBAction func bButton(_ sender: UIButton) {
-        print("B button is clicked...")
-    }
-    @IBAction func cButton(_ sender: UIButton) {
-        print("C button is clicked...")
-    }
-    @IBAction func d_Button(_ sender: UIButton) {
-        print("D button is clicked...")
-    }
-    @IBAction func eButton(_ sender: UIButton) {
-        print("E button is clicked...")
-    }
-//    func playSound(){
-//        let url = Bundle.main.url(forResource: "/Users/yashaswi/Downloads/test", withExtension: "MOV")
-//        player = try! AVAudioPlayer(contentsOf: url!)
-//        player.play()
-//    }
 }
 
